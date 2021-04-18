c64eb92117b7b3040f38b3c4cf327daf8926a19b
Tan Bui <42881707+mewnian-cp@users.noreply.github.com>
2021-04-18 21:03:12 +0700

    add support for input file

---
+++ FrontEnd/demo.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+
+int main() {
+    #ifdef I_DONT_KNOW_WHAT_I_AM_DOING
+    // seriously why are you reading this
+    // 
+    #endif
+}
